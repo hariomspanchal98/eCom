c0f80df7948ec194b961820286a5403e5ba85729
HariOm <hariomspanchal98@gmail.com>
2022-08-18 18:09:14 +0530

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,5 +7,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login';
+  title = 'eCom';
 }
